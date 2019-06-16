b4ab57924a72bcd5f31075288801f3c91229152c
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-06-16 18:06:31 -0400

    new outputs

---
+++ label.sh
@@ -0,0 +1,10 @@
+#bin/bash
+# $1 = dated dir of graph and label, which should be the same
+# $2 = path to new image
+
+python src/label_image.py \
+       --graph=src/graphs/$1 \
+       --labels=src/output_labels/$1 \
+       --input_layer=Placeholder \
+       --output_layer=final_result \
+       --image=$2
